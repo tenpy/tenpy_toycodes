941a553d71356ca434908830e470613d7a070c80
Johannes Hauschild <git@johannes-hauschild.de>
2022-09-05 10:06:50 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# this file allows to install toycodes of this repository, locally with
+#     pip install -e .
+# or online with
+#     pip install git+https://github.com/tenpy/tenpy_toycodes.git
+# to uninstall, simply
+#     pip uninstall tenpy-toycodes
+
+from setuptools import setup
+
+setup(name='tenpy-toycodes',
+      version='0.1.0',
+      packages=['toycodes'])
